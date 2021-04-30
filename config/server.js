076df89aa9faba284f3a0d059827c6be06ec7a5f
Alan Christian <alanchristiancreator@hotmail.com>
2021-04-29 21:09:48 -0300

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd1cc1ad13c074817365420275ec155e1'),
+    },
+  },
+});
